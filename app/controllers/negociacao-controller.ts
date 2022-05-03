ff9154f78ab3d025d5d249a25edeac8b71b0f17b
Alexandre <alexandrerra08@gmail.com>
2022-05-02 22:05:35 -0300

    Aula 3-12 Limpando forms após input

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,6 +15,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociacao;
 
         console.log(negociacao);
+        this.limparFormulario();
     }
 
     criaNegociacao (): Negociacao {
@@ -24,6 +25,13 @@ export class NegociacaoController {
         const valor = parseFloat(this.inputValor.value);
         
         return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 
 }
